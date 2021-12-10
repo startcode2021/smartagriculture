1d2d084cbdd2820fbb22e9fe475176948b9036d7
Administrator <1372383702@qq.com>
2021-12-10 18:03:27 +0800

    haichong finish

--- src/main/java/com/community/controller/IndexController.java
+++ src/main/java/com/community/controller/IndexController.java
@@ -1,18 +1,18 @@
 package com.community.controller;
 
 
+import com.community.dao.pojo.Place;
 import com.community.dao.pojo.User;
+import com.community.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 
@@ -20,8 +20,13 @@ public class IndexController {
 
     @Autowired
     HttpServletRequest request; //通过注解获取一个request
-    @RequestMapping("/index")
-    public String index(){
+    @Autowired
+    PlaceService placeService;
+
+    @RequestMapping("/index/{Place}")
+    public String index(@PathVariable String place,Model model){
+        List<Place> placeList = placeService.list();
+        model.addAttribute("PlaceList",placeList);
         return "index";
     }
     @RequestMapping("/logout")
--- src/main/resources/static/js/index.js
+++
@@ -1,3 +0,0 @@
-$(function (){
-   $('#li1').addClass("active");
-});
