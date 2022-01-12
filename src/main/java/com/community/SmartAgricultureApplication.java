41d85e7dded2cf60dd0424e8311f5ffbc4928525
Administrator <1372383702@qq.com>
2022-01-12 16:34:32 +0800

    1/12 rename

--- src/main/java/com/community/SmartAgricultureApplication.java
+++ src/main/java/com/community/SmartAgricultureApplication.java
@@ -3,16 +3,15 @@ package com.community;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @ImportResource(locations={"classpath:mykaptcha.xml"})
 @MapperScan("com.community.dao.mapper")
 @SpringBootApplication
-public class CommunityApplication {
+public class SmartAgricultureApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommunityApplication.class, args);
+        SpringApplication.run(SmartAgricultureApplication.class, args);
     }
 
 }
--- src/test/java/com/community/SmartAgricultureApplicationTests.java
+++ src/test/java/com/community/SmartAgricultureApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommunityApplicationTests {
+class SmartAgricultureApplicationTests {
 
     @Test
     void contextLoads() {
